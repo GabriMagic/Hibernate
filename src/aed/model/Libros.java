52e02da04275d7a4276217ce57a704b584ac4f3c
GabriMagic <champy9a@gmail.com>
2016-11-29 09:53:08 +0000

    asd

---
+++ src/aed/model/Libros.java
@@ -0,0 +1,62 @@
+package aed.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "libros")
+public class Libros implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int codLibro;
+
+	@Column(columnDefinition = "VARCHAR(40)")
+	private String nombreLibro;
+
+	@Column(columnDefinition = "VARCHAR(20)")
+	private String ISBN;
+
+	private Date fechaIntro;
+
+	public int getCodLibro() {
+		return codLibro;
+	}
+
+	public void setCodLibro(int codLibro) {
+		this.codLibro = codLibro;
+	}
+
+	public String getNombreLibro() {
+		return nombreLibro;
+	}
+
+	public void setNombreLibro(String nombreLibro) {
+		this.nombreLibro = nombreLibro;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public Date getFechaIntro() {
+		return fechaIntro;
+	}
+
+	public void setFechaIntro(Date fechaIntro) {
+		this.fechaIntro = fechaIntro;
+	}
+
+}
