22cabde0af8361347fa161daecf56483c8288c9e
GabriMagic <champy9a@gmail.com>
2016-11-30 19:40:42 +0000

    nj

--- src/aed/controller/MainController.java
+++ src/aed/controller/MainController.java
@@ -2,9 +2,18 @@ package aed.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
 import org.hibernate.Session;
+
+import aed.model.Autor;
+import aed.model.DepositoLegal;
+import aed.model.Ejemplar;
 import aed.model.HibernateUtil;
+import aed.model.Libro;
+import aed.model.LibrosAutores;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -56,34 +65,34 @@ public class MainController implements Initializable {
 		librosTab.setContent(librosController.getLibrosTable());
 		autoresTab.setContent(autorController.getAutoresTable());
 
-		// Libro l1 = new Libro();
-		// l1.setISBN("98-882-8563-F");
-		// l1.setFechaIntro(Date.valueOf(LocalDate.now()));
-		// l1.setNombreLibro("Juego de Tronos");
-		//
-		// Autor a1 = new Autor();
-		// a1.setCodAutor("GAB");
-		// a1.setNombreAutor("GabriMagic");
-		//
-		// Ejemplar ej1 = new Ejemplar();
-		// ej1.setCodEjemplar(1);
-		// ej1.setCodLibro(l1);
-		// ej1.setImporte(95.30);
-		// ej1.setTipo_moneda("EUROS");
-		//
-		// DepositoLegal dl1 = new DepositoLegal();
-		// dl1.setCodLibroDeposito(l1);
-		// dl1.setDepositoLegal("Que va aqui?");
-		//
-		// LibrosAutores la1 = new LibrosAutores();
-		// la1.setCodAutor(a1);
-		// la1.setCodLibro(l1);
-		//
-		// session.save(l1);
-		// session.save(a1);
-		// session.save(ej1);
-		// session.save(dl1);
-		// session.save(la1);
+//		 Libro l1 = new Libro();
+//		 l1.setISBN("98-882-8563-F");
+//		 l1.setFechaIntro(Date.valueOf(LocalDate.now()));
+//		 l1.setNombreLibro("Juego de Tronos");
+//		
+//		 Autor a1 = new Autor();
+//		 a1.setCodAutor("GAB");
+//		 a1.setNombreAutor("GabriMagic");
+//		
+//		 Ejemplar ej1 = new Ejemplar();
+//		 ej1.setCodEjemplar(1);
+//		 ej1.setCodLibro(l1);
+//		 ej1.setImporte(95.30);
+//		 ej1.setTipo_moneda("EUROS");
+//		
+//		 DepositoLegal dl1 = new DepositoLegal();
+//		 dl1.setCodLibroDeposito(l1);
+//		 dl1.setDepositoLegal("Que va aqui?");
+//		
+//		 LibrosAutores la1 = new LibrosAutores();
+//		 la1.setCodAutor(a1);
+//		 la1.setCodLibro(l1);
+//		
+//		 session.save(l1);
+//		 session.save(a1);
+//		 session.save(ej1);
+//		 session.save(dl1);
+//		 session.save(la1);
 
 		session.getTransaction().commit();
 
