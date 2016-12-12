083f7b132ff35829af7667e03985b79143e8e34e
GabriMagic <champy9a@gmail.com>
2016-12-12 08:29:14 +0000

    asd

--- src/aed/model/DepositoLegal.java
+++ src/aed/model/DepositoLegal.java
@@ -5,18 +5,10 @@ import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-
-
-
-
-
 
 @Entity
 @Table(name = "depositoLegal")
@@ -24,9 +16,7 @@ import org.hibernate.annotations.GenericGenerator;
 public class DepositoLegal implements Serializable {
 
 	@Id
-	@GeneratedValue(generator = "myForeign")
-	@GenericGenerator( name = "myForeign", strategy = "foreign ", parameters = {@org.hibernate.annotations.Parameter(name = "property", value = "codLibro")}) 
-	@OneToOne(cascade={CascadeType.PERSIST,CascadeType.REMOVE})
+	@OneToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
 	@JoinColumn(name = "codLibroDeposito")
 	private Libro codLibroDeposito;
 
